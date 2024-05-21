6c039b43b3638eca832e6c5731c4030d769362a7
BongominErickJuma <104322246+BongominErickJuma@users.noreply.github.com>
2024-05-21 18:34:20 +0300

    Daily Bump

---
+++ assets/js/functional/login.js
@@ -0,0 +1,45 @@
+let url = "https://api.cognospheredynamics.com";
+
+const form = document.querySelector(".signin");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+
+  console.log("data: ", data);
+  fetch(`${url}/api/auth/loginUser`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      if (data.error) {
+        const message = document.querySelector(".signin-title");
+        message.textContent = data.error;
+        message.style.color = "salmon";
+        setTimeout(() => {
+          message.textContent = "";
+        }, 4000);
+      }
+
+      // console.log(data.access_token)
+
+      localStorage.setItem("bearer_token", JSON.stringify(data.access_token));
+
+      console.log(JSON.parse(localStorage.getItem("bearer_token")));
+
+      document
+        .querySelector(".signin")
+        .setAttribute("class", "dashboard d.block");
+      document
+        .querySelector(".dashboard")
+        .setAttribute("class", "btn-getstarted d-none");
+
+      window.location.href = `/dashboard.html`;
+    })
+    .catch((error) => console.log(error));
+});
---
+++ assets/js/functional/register.js
@@ -0,0 +1,38 @@
+const messageRegister = () => {
+  document.querySelector(".email").value = "";
+  document.querySelector(".name").value = "";
+  document.querySelector(".phone").value = "";
+  document.querySelector(".password").value = "";
+};
+
+let url = "https://api.cognospheredynamics.com";
+
+const form = document.querySelector(".signup");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+
+  const data = Object.fromEntries(formData);
+  if (data.password === data.confirmPassword) {
+    fetch(`${url}/api/auth/registerUser`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then(() => {
+        messageRegister();
+      })
+      .catch((error) => console.log(error));
+  } else {
+    const message = document.querySelector(".signup-title");
+    message.textContent = "Password didnt match";
+    message.style.color = "salmon";
+
+    setTimeout(() => {
+      message.textContent = "";
+    }, 4000);
+  }
+});
