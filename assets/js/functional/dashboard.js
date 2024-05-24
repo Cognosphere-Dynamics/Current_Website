76af3a55b098d5592e270c0635a7b82aa603c992
BongominErickJuma <104322246+BongominErickJuma@users.noreply.github.com>
2024-05-24 17:00:06 +0300

    daily commit

--- assets/js/functional/dashboard.js
+++ assets/js/functional/dashboard.js
@@ -23,9 +23,11 @@ let default_dashboard = document.getElementById("default_dashboard");
 let aida_ui = document.querySelector(".aida_dashboard");
 let twogere_ui = document.querySelector(".twogere_dashboard");
 let default_ui = document.querySelector(".default_dashboard");
+let payment_form_ui = document.getElementById("payment_form");
 
 aida_ui.style.display = "none";
 twogere_ui.style.display = "none";
+payment_form_ui.style.display = "none";
 
 aida_dashboard.addEventListener("click", () => {
   hide_dashboard();
@@ -40,8 +42,14 @@ twogere_dashboard.addEventListener("click", () => {
   hide_dashboard();
   twogere_ui.style.display = "block";
 });
+
+function show_payment_form() {
+  hide_dashboard();
+  payment_form_ui.style.display = "block";
+}
 function hide_dashboard() {
   aida_ui.style.display = "none";
   default_ui.style.display = "none";
   twogere_ui.style.display = "none";
+  payment_form_ui.style.display = "none";
 }
