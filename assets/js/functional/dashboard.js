a77b9a8737b4c21c0051fc8737061654632feefb
BongominErickJuma <104322246+BongominErickJuma@users.noreply.github.com>
2024-05-26 17:11:41 +0300

    Revert "all changes"

--- assets/js/functional/dashboard.js
+++ assets/js/functional/dashboard.js
@@ -54,105 +54,30 @@ function hide_dashboard() {
   payment_form_ui.style.display = "none";
 }
 
-const offcanvasElement = document.getElementById("offcanvasScrolling");
-const dashboardPages = document.querySelectorAll(".dashboard-page");
-const hideBtns = document.querySelectorAll(".hide-btn");
-const hideText = document.querySelectorAll(".hide-text");
+document.addEventListener("DOMContentLoaded", function () {
+  var offcanvasElement = document.getElementById("offcanvasScrolling");
+  var revealBtn = document.querySelector(".reveal-btn");
+  var dashboardPages = document.querySelectorAll(".dashboard-page");
 
-hideBtns.forEach((hideBtn) => {
-  hideBtn.addEventListener("click", function () {
-    offcanvasElement.classList.toggle("collapsed");
+  offcanvasElement.addEventListener("hidden.bs.offcanvas", function () {
+    dashboardPages.forEach(function (dashboardPage) {
+      dashboardPage.style.margin = "0 auto";
+    });
+    revealBtn.style.visibility = "visible";
+  });
 
-    dashboardPages.forEach((page) => {
-      page.classList.toggle("collapsed");
+  offcanvasElement.addEventListener("shown.bs.offcanvas", function () {
+    dashboardPages.forEach(function (dashboardPage) {
+      dashboardPage.style.marginLeft = "calc(var(--offcanvas-width) + 0.25rem)";
     });
-    hideText.forEach((txt) => {
-      txt.classList.toggle("d-none");
-    });
+    revealBtn.style.visibility = "hidden";
+  });
+
+  revealBtn.addEventListener("click", function () {
+    var offcanvas = new bootstrap.Offcanvas(offcanvasElement);
+    offcanvas.show();
   });
 });
-
-// dashboard_items
-const preview = document.getElementById("preview");
-const conversations = document.getElementById("conversations");
-const finetune_AI = document.getElementById("finetune_AI");
-const account_settings = document.getElementById("account_settings");
-const ai_settings = document.getElementById("ai_settings");
-const adds_marketing = document.getElementById("adds_marketing");
-const dashboard_link = document.getElementById("dashboard_link");
-const conversation_link = document.getElementById("conversation_link");
-const finetune_AI_link = document.getElementById("finetune_AI_link");
-const account_settings_link = document.getElementById("account_settings_link");
-const adds_marketing_link = document.getElementById("adds_marketing_link");
-const ai_settings_link = document.getElementById("ai_settings_link");
-
-function hide_dashboard_items() {
-  preview.style.display = "none";
-  conversations.style.display = "none";
-  finetune_AI.style.display = "none";
-  account_settings.style.display = "none";
-  adds_marketing.style.display = "none";
-  ai_settings.style.display = "none";
-}
-
-hide_dashboard_items();
-preview.style.display = "block";
-dashboard_link.querySelector("a").classList.add("text-primary"); // Highlight the first link by default
-
-dashboard_link.addEventListener("click", (event) => {
-  event.preventDefault(); // Prevent the default behavior of the anchor tag
-  hide_dashboard_items();
-  preview.style.display = "block";
-  removeActiveClassFromLinks();
-  dashboard_link.querySelector("a").classList.add("text-primary");
-});
-
-conversation_link.addEventListener("click", (event) => {
-  event.preventDefault(); // Prevent the default behavior of the anchor tag
-  hide_dashboard_items();
-  conversations.style.display = "block";
-  removeActiveClassFromLinks();
-  conversation_link.querySelector("a").classList.add("text-primary");
-});
-
-finetune_AI_link.addEventListener("click", (event) => {
-  event.preventDefault(); // Prevent the default behavior of the anchor tag
-  hide_dashboard_items();
-  finetune_AI.style.display = "block";
-  removeActiveClassFromLinks();
-  finetune_AI_link.querySelector("a").classList.add("text-primary");
-});
-
-ai_settings_link.addEventListener("click", (event) => {
-  event.preventDefault(); // Prevent the default behavior of the anchor tag
-  hide_dashboard_items();
-  ai_settings.style.display = "block";
-  removeActiveClassFromLinks();
-  ai_settings_link.querySelector("a").classList.add("text-primary");
-});
-
-adds_marketing_link.addEventListener("click", (event) => {
-  event.preventDefault(); // Prevent the default behavior of the anchor tag
-  hide_dashboard_items();
-  adds_marketing.style.display = "block";
-  removeActiveClassFromLinks();
-  adds_marketing_link.querySelector("a").classList.add("text-primary");
-});
-
-account_settings_link.addEventListener("click", (event) => {
-  event.preventDefault(); // Prevent the default behavior of the anchor tag
-  hide_dashboard_items();
-  account_settings.style.display = "block";
-  removeActiveClassFromLinks();
-  account_settings_link.querySelector("a").classList.add("text-primary");
-});
-
-function removeActiveClassFromLinks() {
-  const navLinks = document.querySelectorAll(".nav-link");
-  navLinks.forEach((link) => {
-    link.classList.remove("text-primary");
-  });
-}
 
 // const timeout = 1 * 30 * 1000;
 // setTimeout(() => {
