f9177897df79f174fc2bd14de8ad05b0d95c3273
BongominErickJuma <104322246+BongominErickJuma@users.noreply.github.com>
2024-06-02 20:33:58 +0300

    changes

--- assets/js/functional/register.js
+++ assets/js/functional/register.js
@@ -1,120 +1,111 @@
-const emailValue = document.querySelector(".email").value;
-const proceedBtn = document.querySelector(".proceed_btn1");
-const otpFeedBack = document.querySelector(".otp-box");
-const loader = document.querySelector(".loader");
-const otpInput = document.querySelector(".otp-input");
-const formBox = document.querySelector(".form-box");
-const otp = document.getElementById("otp");
-const submitBtn = document.querySelector(".submit-btn");
-const otpText = document.querySelector(".title-otp");
-const submitDiv = document.querySelector(".submit-div");
-const editEmail = document.querySelector(".editEmail");
+const proceedBtn = document.getElementById("proceedBtn");
+const submitBtn = document.getElementById("submitButton");
 
-editEmail.addEventListener("click", () => {
-  location.reload();
-  formBox.classList.toggle("disabled-div");
-});
+proceedBtn.addEventListener("click", function () {
+  let form = document.getElementById("signupForm");
+  let data = new FormData(form);
+  const formData = Object.fromEntries(data);
 
-proceedBtn.addEventListener("click", () => {
-  const emailValue = document.querySelector(".email").value;
-  const username = document.querySelector(".username").value;
-  const name = document.querySelector(".name").value;
-  const phone = document.querySelector(".phone").value;
-  const password = document.querySelector(".password").value;
-  const confirmPassword = document.querySelector(".confirm-password").value;
+  let emailField = document.querySelector('input[name="email"]');
 
-  const allFieldsFilled =
-    emailValue && username && phone && password && confirmPassword;
-  const passwordsMatch = password === confirmPassword;
+  // check if email is right
 
-  if (otp.value.trim() === "") {
-    if (allFieldsFilled) {
-      if (passwordsMatch) {
-        otpFeedBack.classList.remove("d-none");
-        editEmail.classList.remove("d-none");
-        formBox.classList.add("disabled-div");
+  function validateEmail(email) {
+    const re =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@(([^<>()[\]\.,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,})$/i;
+    return re.test(String(email).toLowerCase());
+  }
 
-        // send mail for verification
+  if (!validateEmail(emailField.value)) {
+    alert("Invalid email");
+    return;
+  }
 
-        const userRegister = {
-          email: emailValue,
-          name,
-          username,
-          phone,
-          password,
-        };
-        fetch(
-          `https://api.cognospheredynamics.com/api/auth/sendMailForVerification`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(userRegister),
-          }
-        )
-          .then((res) => res.json())
-          .then((data) => {
-            if (data.success) {
-              loader.classList.add("d-none");
-              otpInput.classList.remove("d-none");
-            } else {
-              loader.classList.add("d-none");
-              otpInput.classList.remove("d-none");
-              otp.classList.add("d-none");
-              let errorMessage = data.errors.email[0];
-              otpText.textContent = errorMessage;
-              otpText.style.color = "red";
-              // return;
-            }
-          })
-          .catch((error) => {
-            console.log("Error:", error);
-            emailFeedBack.textContent = error.TypeError;
-          });
-      } else {
-        const message = document.querySelector(".signup-title");
-        message.textContent = "Password didn't match";
-        message.style.color = "salmon";
-      }
-    } else {
-      const message = document.querySelector(".signup-title");
-      message.textContent = "All Fields are required";
-      message.style.color = "salmon";
-    }
-  } else {
-    otpFeedBack.classList.remove("d-none");
-    loader.classList.remove("d-none");
+  // Hide the submit button and show loader
+  document.getElementById("submitButton").classList.add("d-none");
+  document.getElementById("loaderSection").classList.remove("d-none");
 
-    const verifyingObject = {
-      email: emailValue,
-      velification_code: otp.value.trim(),
-    };
-    fetch(`https://api.cognospheredynamics.com/api/auth/verifyingCode`, {
+  // Perform the first step of form submission
+
+  fetch(
+    "https://api.cognospheredynamics.com/api/auth/sendMailForVerification",
+    {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(verifyingObject),
+      body: JSON.stringify(formData),
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      // Handle response
+      if (data.success) {
+        // Show OTP section
+        document.getElementById("otpSection").classList.remove("d-none");
+        document.getElementById("submitButton").classList.remove("d-none");
+        document.getElementById("proceedBtn").classList.add("d-none");
+      } else {
+        alert(data.errors.email[0]);
+        emailField.value = "";
+      }
+      // Hide loader and show submit button
+      document.getElementById("loaderSection").classList.add("d-none");
     })
-      .then((res) => res.json())
-      .then((data) => {
-        otpInput.classList.remove("d-none");
+    .catch((error) => {
+      console.log(error);
+      // // Hide loader and show submit button
+      document.getElementById("loaderSection").classList.add("d-none");
+      document.getElementById("submitButton").classList.remove("d-none");
+    });
+});
 
-        if (data.success) {
-          submitDiv.classList.remove("d-none");
-          otpInput.classList.add("d-none");
-          otpFeedBack.classList.add("d-none");
-          loader.classList.add("d-none");
-          window.location.href = `./sphere_signin.html`;
-        } else {
-          otpInput.classList.add("d-none");
-          otpFeedBack.classList.add("d-none");
-          otpText.textContent = "Wrong OTP";
-        }
-      })
-      .catch((error) => {
-        console.log("Error:", error);
-      });
+// Add an event listener to handle OTP submission
+submitBtn.addEventListener("click", function () {
+  // Function to check if all fields are filled
+
+  var otp = document.getElementById("otp").value;
+  let form = document.getElementById("signupForm");
+  let data = new FormData(form);
+  const formData = Object.fromEntries(data);
+  formData.velification_code = otp;
+
+  console.log(formData);
+
+  function areAllFieldsFilled(formData) {
+    for (let key in formData) {
+      if (formData[key].trim() === "") {
+        return false;
+      }
+    }
+    return true;
   }
+
+  if (!areAllFieldsFilled(formData)) {
+    alert("all Fields are required");
+    return;
+  }
+
+  // Perform OTP verification
+  fetch("https://api.cognospheredynamics.com/api/auth/verifyingCode", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Handle response
+      if (data.success) {
+        window.location.href = `./sphere_signin.html`;
+        // Redirect or perform further actions
+      } else {
+        alert("Wrong OTP try again");
+      }
+    })
+    .catch((error) => {
+      alert("Error: ", error);
+    });
 });
